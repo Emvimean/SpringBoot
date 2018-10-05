cdd9ee2b7fa4d90c2f0e3724b58056d820426e6b
Emvimean <32316801+Emvimean@users.noreply.github.com>
2018-10-05 22:12:41 +0700

    WebMvcConfig

--- src/main/java/com/sengmean/configuration/WebMcvConfiguration.java
+++ src/main/java/com/sengmean/configuration/WebMcvConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class WebMcvConfiguration extends WebMvcConfigurerAdapter {
 
-
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		super.addViewControllers(registry);
